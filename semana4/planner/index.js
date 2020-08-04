718410e8287a6d8bfbb48faee53a09ca960e7ff9
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-08-04 17:00:56 -0300

    Começo do exercício de DOM.

---
+++ semana4/planner/index.js
@@ -0,0 +1,12 @@
+function adicionarTarefa() {
+    const guardaTarefa = document.getElementById("tarefa");
+    let tarefaDigitada = guardaTarefa.value;
+
+    const diaDaSemana = document.getElementById("dias-semana");
+
+    switch (diaDaSemana) {
+        case "domingo":
+            const tarefaDoDia = document.getElementById("domingo");
+        
+    }
+}
