809054f38680bcede58f5acdf5dbc0ed4f035fc7
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-08-05 10:39:52 -0300

    Limpando campo de input.

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,47 +1,47 @@
 function adicionarTarefa() {
-    const guardaTarefa = document.getElementById("tarefa").value;
+    const guardaTarefa = document.getElementById("tarefa");
     const diaDaSemana = document.getElementById("dias-semana").value;
 
     switch (diaDaSemana) {
         case "domingo":
             const tarefaDomingo = document.getElementById("domingo");
-            tarefaDomingo.innerHTML += `<li>${guardaTarefa}</li>`;
+            tarefaDomingo.innerHTML += `<li>${guardaTarefa.value}</li>`;
             guardaTarefa.value = "";
             break;
 
         case "segunda":
             const tarefaSegunda = document.getElementById("segunda");
-            tarefaSegunda.innerHTML += `<li>${guardaTarefa}</li>`;
+            tarefaSegunda.innerHTML += `<li>${guardaTarefa.value}</li>`;
             guardaTarefa.value = "";
             break;
 
         case "terca":
             const tarefaTerca = document.getElementById("terca");
-            tarefaTerca.innerHTML += `<li>${guardaTarefa}</li>`;
+            tarefaTerca.innerHTML += `<li>${guardaTarefa.value}</li>`;
             guardaTarefa.value = "";
             break;
 
         case "quarta":
             const tarefaQuarta = document.getElementById("quarta");
-            tarefaQuarta.innerHTML += `<li>${guardaTarefa}</li>`;
+            tarefaQuarta.innerHTML += `<li>${guardaTarefa.value}</li>`;
             guardaTarefa.value = "";
             break;
 
         case "quinta":
             const tarefaQuinta = document.getElementById("quinta");
-            tarefaQuinta.innerHTML += `<li>${guardaTarefa}</li>`;
+            tarefaQuinta.innerHTML += `<li>${guardaTarefa.value}</li>`;
             guardaTarefa.value = "";
             break;
 
         case "sexta":
             const tarefaSexta = document.getElementById("sexta");
-            tarefaSexta.innerHTML += `<li>${guardaTarefa}</li>`;
+            tarefaSexta.innerHTML += `<li>${guardaTarefa.value}</li>`;
             guardaTarefa.value = "";
             break;
 
         case "sabado":
             const tarefaSabado = document.getElementById("sabado");
-            tarefaSabado.innerHTML += `<li>${guardaTarefa}</li>`;
+            tarefaSabado.innerHTML += `<li>${guardaTarefa.value}</li>`;
             guardaTarefa.value = "";
             break;
     }
