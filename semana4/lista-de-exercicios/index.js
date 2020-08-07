60d3a06a92a23e69eca04578c3790589b4d00d13
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-08-07 11:08:04 -0300

    Interpretação - Exercício 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,13 @@
+//Exercícios de interpretação de código
+
+//Exercício 1
+/* É criada uma função (conversorDeMoeda) que recebe como parâmetro um valor (valorEmDolar).
+    Dentro desta função, existe uma variável (cotacao) que recebe o valor da cotacão do dólar indicado pelo
+    usuário e converte esse valor de string para número usando o cast Number. Após isso, a função retorna o
+    começo de uma string como R$ mais a operação que multiplica o valor passado no parâmetro da função pelo
+    valor de cotação indicado pelo usuário.
+    Depois, fora da função, é criada uma variável (meuDinheiro) que recebe a função em si com o valor 100
+    passado como parâmetro e em seguido é impresso no console o resultado armazenado na variável meuDinheiro.
+    Se, por exemplo, o valor de cotação digitado pelo usuário for 5, então o console vai apresentar o 
+    resultado R$500. */
+
