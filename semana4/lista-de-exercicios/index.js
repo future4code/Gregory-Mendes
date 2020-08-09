460becdaeec204bc0d952666022219960be0834d
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-08-09 19:45:29 -0300

    Funções de array - Exercícío 3A.

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -291,7 +291,7 @@ console.log(mostraAdolescentes); */
 
 //Exercício 2
 
-const array = [1, 2, 3, 4, 5, 6];
+//const array = [1, 2, 3, 4, 5, 6];
 
 // A
 /* 
@@ -320,3 +320,28 @@ array.forEach((numero, index) => {
 });
 
 console.log(array); */
+
+//Exercícío 3
+
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+// A
+/* 
+const mostraAutorizados = pessoas.filter((pessoa) => {
+    if (pessoa.idade >= 14 && pessoa.idade <= 60 && pessoa.altura > 1.5) {
+        return true;
+    }
+    
+    return false;
+})
+
+console.log(mostraAutorizados); */
+
+// B
