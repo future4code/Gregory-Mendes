32219bb08cb1e82cd6a0d42074040ed9dcb19013
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-08-09 21:21:56 -0300

    Funções de array - Exercício 5.

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -398,3 +398,26 @@ const confirmarConsulta = consultas.map((paciente, index) => {
 })
 
 console.log (confirmarConsulta); */
+
+//Exercícío 5
+/*
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+contas.forEach((cliente) => {
+    let somaCompras = 0;
+    for (let valor of cliente.compras) {
+        somaCompras += valor;
+    }
+
+    cliente.saldoTotal -= somaCompras;
+})
+
+console.log(contas);
+*/
