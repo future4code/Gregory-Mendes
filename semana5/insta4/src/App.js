f7c3940d58d5b3894dcf448c25f5cd2ee640657d
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-08-13 17:17:36 -0300

    Atividade React - Aula 20.

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -3,6 +3,28 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    posts: [
+      {
+        nomeUsuario: 'paulinha',
+        fotoUsuario:'https://picsum.photos/50/50',
+        fotoPost:'https://picsum.photos/200/150'
+      },
+
+      {
+        nomeUsuario:'greg',
+        fotoUsuario:'https://images.uncyc.org/pt/thumb/a/a4/Alpaca.jpg/220px-Alpaca.jpg',
+        fotoPost: 'https://s3.amazonaws.com/ogden_images/www.lockhaven.com/images/2019/03/05214950/Huacaya-Alpacas.jpg'
+      },
+
+      {
+        nomeUsuario={'reysla'},
+        fotoUsuario={'https://media.gazetadopovo.com.br/vozes/2012/11/chow_chow-53764208.jpg'},
+        fotoPost={'https://64.media.tumblr.com/50a3959ae18cf668a08761f3f6bf4741/tumblr_pq7wjzVHio1qj69eg_540.png'}
+      },
+    ]
+  }
+  
   render() {
     return (
       <div className={'app-container'}>
