5c1874f55f78815985f1de6b40eb9cc3a0521bf8
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-09-10 17:00:50 -0300

    Botões de match com a função integrada na API de Choose Person funcionando.

--- semana9/astromatch/src/screens/MatchPage/MatchPage.js
+++ semana9/astromatch/src/screens/MatchPage/MatchPage.js
@@ -3,27 +3,52 @@ import axios from 'axios'
 import { choosePersonUrl, getProfileToChooseUrl } from '../../constants/axiosConfig'
 
 const MatchPage = (props) => {
+    const [idValue, setId] = useState("")
     const [ageValue, setAge] = useState(0);
     const [nameValue, setName] = useState("");
     const [photoValue, setPhoto] = useState("");
     const [bioValue, setBio] = useState("");
-    const [choiceValue, setChoice] = useState(false);
 
     useEffect(() => {
-        getPersonProfile()
+        getNewPersonProfile(idValue);
     }, [])
     
-    const getPersonProfile = () => {
+    const getNewPersonProfile = () => {
         axios.get(getProfileToChooseUrl)
         .then((response) => {
-            setAge(response.data.profile.age)
-            setName(response.data.profile.name)
-            setPhoto(response.data.profile.photo)
-            setBio(response.data.profile.bio)
-            console.log(response.data)
+            setId(response.data.profile.id)
+            setAge(response.data.profile.age);
+            setName(response.data.profile.name);
+            setPhoto(response.data.profile.photo);
+            setBio(response.data.profile.bio);
         })
         .catch((error) => {
-            console.log(error.data)
+            console.log(error.data);
+        })
+    }
+
+    const choosePerson = (userChoice, personId) => {
+        let body;
+
+        if (userChoice === "match") {
+            body = {
+                id: personId,
+                choice: true
+            };
+        } else if (userChoice === "dont-match") {
+            body = {
+                id: personId,
+                choice: false
+            };
+        }
+
+        axios.post(choosePersonUrl, body, {headers:{'Content-Type': "application/json"}})
+        .then((response) => {
+            getNewPersonProfile();
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.log(error.message);
         })
     }
 
@@ -32,13 +57,13 @@ const MatchPage = (props) => {
             <h1>Astromatch</h1>
             <button onClick={props.onClickListButton}>Lista de matches</button>
             <div>
-                <img src={photoValue} width="300px" height="300px"/>
+                <img src={photoValue} width="400px" height="400px"/>
                 <p>Nome: {nameValue}</p>
                 <p>Idade: {ageValue}</p>
                 <p>Descrição: {bioValue}</p>
             </div>
-            <button>X</button>
-            <button>✓</button>
+            <button onClick={() => choosePerson("dont-match", idValue)}>X</button>
+            <button onClick={() => choosePerson("match", idValue)}>✓</button>
         </div>
     )
 }
