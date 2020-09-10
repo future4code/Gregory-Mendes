8deef81278ea94e242d3ec25dbad3b2eb551fb9c
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-09-10 14:52:59 -0300

    Integração com a API para exibir pessoas na tela de match.

---
+++ semana9/astromatch/src/constants/axiosConfig.js
@@ -0,0 +1,4 @@
+export const choosePersonUrl = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/gregory/choose-person"
+export const getProfileToChooseUrl = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/gregory/person"
+export const getMatchesUrl = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/gregory/matches"
+export const clearUrl = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/gregory/clear"
--- semana9/astromatch/src/screens/MatchPage/MatchPage.js
+++ semana9/astromatch/src/screens/MatchPage/MatchPage.js
@@ -1,17 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import { choosePersonUrl, getProfileToChooseUrl } from '../../constants/axiosConfig'
 
 const MatchPage = (props) => {
+    const [ageValue, setAge] = useState(0);
+    const [nameValue, setName] = useState("");
+    const [photoValue, setPhoto] = useState("");
+    const [bioValue, setBio] = useState("");
+    const [choiceValue, setChoice] = useState(false);
+
+    useEffect(() => {
+        getPersonProfile()
+    }, [])
     
+    const getPersonProfile = () => {
+        axios.get(getProfileToChooseUrl)
+        .then((response) => {
+            setAge(response.data.profile.age)
+            setName(response.data.profile.name)
+            setPhoto(response.data.profile.photo)
+            setBio(response.data.profile.bio)
+            console.log(response.data)
+        })
+        .catch((error) => {
+            console.log(error.data)
+        })
+    }
+
     return (
         <div>
             <h1>Astromatch</h1>
             <button onClick={props.onClickListButton}>Lista de matches</button>
             <div>
-                <img/>
-                <p>Nome:</p>
-                <p>Idade:</p>
-                <p>Descrição:</p>
+                <img src={photoValue} width="300px" height="300px"/>
+                <p>Nome: {nameValue}</p>
+                <p>Idade: {ageValue}</p>
+                <p>Descrição: {bioValue}</p>
             </div>
             <button>X</button>
             <button>✓</button>
