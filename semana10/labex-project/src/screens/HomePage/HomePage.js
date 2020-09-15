fab797e3f19b07401fb5dc74a984273d117bb7c4
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-09-15 16:52:56 -0300

    Adicionando botões de Login e Aplicação para viagem.

--- semana10/labex-project/src/screens/HomePage/HomePage.js
+++ semana10/labex-project/src/screens/HomePage/HomePage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { goToApplicationPage, goToLoginPage } from '../../routers/switchPages';
+import { useHistory } from 'react-router-dom'
 
 const HomePage = () => {
-    
+    const history = useHistory();
+
     return (
         <div>
             <p>Home</p>
+            <button onClick={() => goToApplicationPage(history)}>Candidatar agora!</button>
+            <button onClick={() => goToLoginPage(history)}>Fazer login</button>
         </div>
     )
 }
