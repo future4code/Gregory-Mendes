2f936d593ee567be5020e7fbdbdbd0598b2f5d3a
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-09-23 18:01:48 -0300

    Exercício 3

--- semana11/testes-unitarios/src/exercicios/ex3.js
+++ semana11/testes-unitarios/src/exercicios/ex3.js
@@ -1,13 +1,16 @@
+import { createTrue } from "typescript";
+
 export function checaItensDuplicados(array) {
   
   for (let i = 0; i < array.length; i++) {
     for(let j = 0; j < array.length; j++) {
       if (i !== j) {
         if (array[i] === array[j]) {
-          return array[i] === array[j];
+          return true;
         }
       }
     }
   }
 
+  return false;
 }
