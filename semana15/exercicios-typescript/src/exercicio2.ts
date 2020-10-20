696695ce400ed77122e4aaf3b9fc2df3d2cd17ab
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-10-20 16:49:35 -0300

    Exercício 1 completo e parte do Exercício 2.

---
+++ semana15/exercicios-typescript/src/exercicio2.ts
@@ -0,0 +1,30 @@
+// A, B e C
+// A - A entrada da funcão é um array de números e a saída são as estatisticas (maior, menor e média).
+// B - As outras variáveis são: numerosOrdenados (number[]), parâmetros do sort (a e b são do tipo number), soma (number), estatísticas(Object).
+function obterEstatisticas(numeros: number[]) {
+
+    const numerosOrdenados: number[] = numeros.sort(
+        (a: number, b: number) => a - b
+    );
+
+    let soma: number = 0;
+
+    for (let num of numeros) {
+        soma += num;
+    };
+
+    const estatisticas: Object = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    };
+
+    return estatisticas;
+}
+
+const amostraDeNumeros = {
+
+    numeros: [21, 18, 65, 44, 15, 18],
+
+    obterEstatisticas: (numeros) => {...}
+}
