5aa0346130dcfbe926ede68083902ccfe6cc5835
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-10-21 20:18:13 -0300

    Endpoint 3 completo.

--- semana15/exercicios-express/src/endpoints.ts
+++ semana15/exercicios-express/src/endpoints.ts
@@ -18,6 +18,38 @@ app.get("/countries/All", (req: Request, res: Response) => {
     res.status(200).send(result);
 });
 
+app.get("/countries/search", (req: Request, res: Response) => {
+    let result: country[] | undefined = countries;
+
+    try {
+        if(!req.query.name && !req.query.capital && !req.query.continent) {
+            throw new Error();
+        };
+
+        if (req.query.name) {
+            result = result.filter((country) => 
+                country.name.toLowerCase().includes(req.query.name as string)
+            );
+        };
+        
+        if (req.query.capital) {
+            result = result.filter((country) => 
+                country.capital.toLowerCase().includes(req.query.capital as string)
+            ); 
+        };
+        
+        if (req.query.continent) {
+            result = result.filter((country) => 
+                country.continent.toLowerCase().includes(req.query.continent as string)
+            );
+        };
+        
+        res.status(200).send(result);
+    } catch (error) {
+        res.status(404).send("Parâmetros não foram preenchidos!");
+    }
+});
+
 app.get("/countries/:id", (req: Request, res: Response) => {
     const result: country | undefined = countries.find((country) =>
         country.id === Number(req.params.id)
@@ -29,6 +61,7 @@ app.get("/countries/:id", (req: Request, res: Response) => {
         res.status(404).send("Not found");
     };
 });
+
 
 app.listen(3004, () => {
     console.log("Servidor pronto!");
