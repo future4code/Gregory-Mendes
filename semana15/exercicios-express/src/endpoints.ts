25b8103d112057cfeacb48c032a21d2f1802fd73
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-10-22 15:24:52 -0300

    Endpoint 4.

--- semana15/exercicios-express/src/endpoints.ts
+++ semana15/exercicios-express/src/endpoints.ts
@@ -62,6 +62,34 @@ app.get("/countries/:id", (req: Request, res: Response) => {
     };
 });
 
+app.put("/countries/edit/:id", (req: Request, res: Response) => {
+    
+    try {
+        type body = {
+            name: string,
+            capital: string
+        }
+
+        const {name, capital}: body = req.body;
+        
+    
+        const countryIndex = countries.findIndex((country) =>
+            country.id === Number(req.params.id)
+        );
+    
+        if (countryIndex === -1) {
+            throw new Error();
+        }
+    
+        countries[countryIndex].name = name;
+        countries[countryIndex].capital = capital;
+    
+        res.status(200).send({message: "O país foi editado com sucesso!"})
+        
+    } catch (error) {
+        res.status(400).send({message: "Erro ao tentar editar o país."})
+    }
+});
 
 app.listen(3004, () => {
     console.log("Servidor pronto!");
