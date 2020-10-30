0f3f3ae16a4701bd2f4e9a687c9064464eac875c
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-10-30 17:47:25 -0300

    Configurações iniciais e endpoint de criar usuário.

---
+++ semana16/to-do-list-project/src/data/createNewUser.ts
@@ -0,0 +1,21 @@
+import { connection } from '../index'
+
+export async function createNewUser(
+    name: string,
+    nickname: string,
+    email: string
+): Promise<void> {
+    try {
+        await connection
+        .insert({
+            name,
+            nickname,
+            email
+        })
+        .into("ToDoListUser")
+
+        console.log("Usuário criado com sucesso");
+    } catch (error) {
+        console.log(error)
+    };
+};
